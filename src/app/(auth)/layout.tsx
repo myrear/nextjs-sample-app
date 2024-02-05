8bcbde5f8af74f982d477837cdea63f47a7a62cb
myrear <mikuri.nakano@terass.com>
2024-02-05 00:52:41 +0000

    type

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import { auth } from '@/auth'
 import { ReactNode } from 'react'
 
-export default async function AuthLayout(props: Record<string, ReactNode>) {
+export default async function AuthLayout(props: Record<'children', ReactNode>) {
   const session = await auth()
 
   return <>{session ? props.children : 'サインインして'}</>
--- src/app/(auth)/todo/layout.tsx
+++ src/app/(auth)/todo/layout.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react'
 
-export default function TodosLayout(props: Record<string, ReactNode>) {
+export default function TodosLayout(
+  props: Record<'children' | 'addTodoForm' | 'modal', ReactNode>
+) {
   return (
     <>
       {props.children}
